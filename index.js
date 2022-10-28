80ff866fb9ced2fe94874c4fbc663601b80ab3a2
Jason Qiu <jasonqiu93@gmail.com>
2022-10-27 17:39:24 -0700

    ... might be screwing things up :D

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+let index = 0; 
+let images = document.getElementsByClassName("bgImage")
 
+function changeImg(){
+    images[i].class = "bgImage inactive";
+
+    if(i == images.length){
+        i = 0
+    }
+    images[i].class = "bgImage";
+}
+
+setInterval(5000, changeImg);
