21f4966c104900d147a0b3385783d313900e82ee
Jason Qiu <jasonqiu93@gmail.com>
2022-10-29 21:03:17 -0700

    navbar dissapear when go down... no animation tho :/

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ setInterval(changeImg, 10000);
 
 // probably stop process after they scroll off. less slow maybe? 
 
+
+
+var prevScrollpos = window.scrollY;
+
+
+
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navBarDiv").style.top = "0";
+  } else {
+    document.getElementById("navBarDiv").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
