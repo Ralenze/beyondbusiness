8a03e01269984804016157de2f4153a034c82439
Jason Qiu <jasonqiu93@gmail.com>
2022-10-31 22:52:00 -0700

    idk...

--- SectionHeaderStickyThing.js
+++ SectionHeaderStickyThing.js
@@ -2,7 +2,7 @@ const aboutHeader = document.getElementById("aboutHeader")
 const startY = aboutHeader.offsetTop; 
 
 function onScroll(){
-    if(window.scrollY > startY + 350){
+    if(window.scrollY > startY + 340){
         aboutHeader.classList.add("activeHeader");
     }else{
         aboutHeader.classList.remove("activeHeader");
