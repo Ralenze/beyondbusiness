2cdecac9a46c916e16a3b7f58d38335a37021215
Jason Qiu <jasonqiu93@gmail.com>
2022-10-31 22:19:41 -0700

    idk same isse as before. whitespace bottom page... div not filling

--- SectionHeaderStickyThing.js
+++ SectionHeaderStickyThing.js
@@ -1,7 +1,8 @@
 const aboutHeader = document.getElementById("aboutHeader")
+const startY = aboutHeader.offsetTop; 
 
 function onScroll(){
-    if(window.scrollY > 700 && window.scrollY ){
+    if(window.scrollY > startY + 350){
         aboutHeader.classList.add("activeHeader");
     }else{
         aboutHeader.classList.remove("activeHeader");
