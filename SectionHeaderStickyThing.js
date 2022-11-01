f00810ad9e0d85cd35602c0c189945eb0d2a0537
Jason Qiu <jasonqiu93@gmail.com>
2022-10-31 21:50:51 -0700

    new issue... bottom of thing is white

--- SectionHeaderStickyThing.js
+++ SectionHeaderStickyThing.js
@@ -1,7 +1,7 @@
 const aboutHeader = document.getElementById("aboutHeader")
 
 function onScroll(){
-    if(window.scrollY > 750 && window.scrollY < 2000 ){
+    if(window.scrollY > 700 && window.scrollY < 2000 ){
         aboutHeader.classList.add("activeHeader");
     }else{
         aboutHeader.classList.remove("activeHeader");
