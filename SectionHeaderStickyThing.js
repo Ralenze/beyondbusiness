fd0c7c3330bfbd1c99073a349ef39b63efadb04d
Jason Qiu <jasonqiu93@gmail.com>
2022-10-31 21:59:05 -0700

    ima be honest idk what i did...

--- SectionHeaderStickyThing.js
+++ SectionHeaderStickyThing.js
@@ -1,7 +1,7 @@
 const aboutHeader = document.getElementById("aboutHeader")
 
 function onScroll(){
-    if(window.scrollY > 700 && window.scrollY < 2000 ){
+    if(window.scrollY > 700 && window.scrollY ){
         aboutHeader.classList.add("activeHeader");
     }else{
         aboutHeader.classList.remove("activeHeader");
