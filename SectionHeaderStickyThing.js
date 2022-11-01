f56fd89bb1475c775aa9873c5c160c8a3e6c73d7
Jason Qiu <jasonqiu93@gmail.com>
2022-11-01 11:16:12 -0700

    weeeeeee

--- SectionHeaderStickyThing.js
+++ SectionHeaderStickyThing.js
@@ -1,8 +1,11 @@
 const aboutHeader = document.getElementById("aboutHeader")
+const text = document.getElementsByClassName("aboutText");
 const startY = aboutHeader.offsetTop; 
+const endY = text[text.length-1].offsetTop
 
+// get way measure size of title and final paragraph ig
 function onScroll(){
-    if(window.scrollY > startY + 340){
+    if(window.scrollY > startY + 340 && window.scrollY <= endY + 560){
         aboutHeader.classList.add("activeHeader");
     }else{
         aboutHeader.classList.remove("activeHeader");
