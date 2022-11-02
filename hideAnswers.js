8c6ba3ce4c5668fe8412beb34ace617162659aac
Jason Qiu <jasonqiu93@gmail.com>
2022-11-02 10:30:00 -0700

    ...

--- hideAnswers.js
+++ hideAnswers.js
@@ -5,6 +5,8 @@ for(i = 0; i< questions.length;i++){
     questions[i].addEventListener("click",() =>{
 
         panel = this.nextElementSibling; 
+        // issue is panel i think... like this isn't selecting properly cus error is that 
+        // .style is undefined so i think isn't selecting thing want...
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
           } else {
