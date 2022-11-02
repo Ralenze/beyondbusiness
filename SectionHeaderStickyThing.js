f3f8b4b2367165d264aac3152eb1b36717ec5331
Jason Qiu <jasonqiu93@gmail.com>
2022-11-01 17:57:04 -0700

    idk anymore man...

--- SectionHeaderStickyThing.js
+++ SectionHeaderStickyThing.js
@@ -1,11 +1,12 @@
 const aboutHeader = document.getElementById("aboutHeader")
 const text = document.getElementsByClassName("aboutText");
-const startY = aboutHeader.offsetTop; 
-const endY = text[text.length-1].offsetTop
+const startY = aboutHeader.offsetTop + aboutHeader.offsetHeight*4
+const endY = text[text.length-1].offsetTop + text[text.length-1].offsetHeight
 
-// get way measure size of title and final paragraph ig
+// figure out better way decide when tf start and end...
+// also animations or smth? idfl...
 function onScroll(){
-    if(window.scrollY > startY + 340 && window.scrollY <= endY + 560){
+    if(window.scrollY > startY  && window.scrollY <= endY + 560){
         aboutHeader.classList.add("activeHeader");
     }else{
         aboutHeader.classList.remove("activeHeader");
---
+++ hideAnswers.js
@@ -0,0 +1,16 @@
+questions = document.getElementsByClassName("questionHeader")
+
+for(i = 0; i< questions.length;i++){
+    questions[i].addEventListener("click",() =>{
+        this.innerHTML = "hi";
+
+        var panel = this.nextElementSibling; 
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+    })
+}
+
+// btw the thing isn't a button so idk if there are issues adding eventlistener to it...
