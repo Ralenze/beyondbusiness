b8f24fbf35837aa091b9da6947d4ddb5386acd76
Jason Qiu <jasonqiu93@gmail.com>
2022-11-07 14:24:22 -0800

    same

--- SectionHeaderStickyThing.js
+++ SectionHeaderStickyThing.js
@@ -1,12 +1,14 @@
 const aboutHeader = document.getElementById("aboutHeader")
 const text = document.getElementsByClassName("aboutText");
-const startY = aboutHeader.offsetTop + aboutHeader.offsetHeight*4
-const endY = text[text.length-1].offsetTop + text[text.length-1].offsetHeight
+const startY = aboutHeader.offsetTop + aboutHeader.offsetHeight*7
+// figure out how to this better...
+
+const endY = text[text.length-1].offsetTop + text[text.length-1].offsetHeight*3
 
 // figure out better way decide when tf start and end...
 // also animations or smth? idfl...
 function onScroll(){
-    if(window.scrollY > startY  + 300 && window.scrollY <= endY){
+    if(window.scrollY > startY && window.scrollY <= endY){
         aboutHeader.classList.add("activeHeader");
     }else{
         aboutHeader.classList.remove("activeHeader");
