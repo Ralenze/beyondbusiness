1812fd3fb8e5c7b357c4f7ed50a0b5b89c956675
Jason Qiu <jasonqiu93@gmail.com>
2022-11-07 21:54:00 -0800

    working on about Sticky Header... need figure out how to choose placement for it to stay at when done  rn just guessed ...

--- SectionHeaderStickyThing.js
+++ SectionHeaderStickyThing.js
@@ -1,16 +1,22 @@
 const aboutHeader = document.getElementById("aboutHeader")
 const text = document.getElementsByClassName("aboutText");
-const startY = aboutHeader.offsetTop + aboutHeader.offsetHeight*7
-// figure out how to this better...
+const clientHeight = document.documentElement.clientHeight;
+const startY = aboutHeader.offsetParent.offsetTop + aboutHeader.offsetTop + aboutHeader.offsetHeight - 0.6* clientHeight;
 
-const endY = text[text.length-1].offsetTop + text[text.length-1].offsetHeight*3
 
-// figure out better way decide when tf start and end...
+const endY = text[text.length-1].offsetParent.offsetTop+ text[text.length-1].offsetTop -0.4 * clientHeight;
+// figure out how to this better..
+
+
 // also animations or smth? idfl...
 //also also, have thing stay at end position when done? 
 function onScroll(){
-    if(window.scrollY > startY && window.scrollY <= endY){
+    if(window.scrollY > startY && window.scrollY < endY){
         aboutHeader.classList.add("activeHeader");
+        aboutHeader.classList.remove("endHeader");
+    }else if(window.scrollY>= endY){
+        aboutHeader.classList.remove("activeHeader");
+        aboutHeader.classList.add("endHeader");
     }else{
         aboutHeader.classList.remove("activeHeader");
     }
@@ -19,6 +25,6 @@ function onScroll(){
 window.addEventListener('scroll', onScroll)
 
 
-
+// supposed be around 830
 
 
