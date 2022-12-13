aa185f7612b07274288f97bc42163c39c9cfda48
Jason Qiu <jasonqiu93@gmail.com>
2022-12-13 00:07:30 -0800

    ngl website pretty bad- welp.... sorry :D

--- hideAnswers.js
+++ hideAnswers.js
@@ -4,18 +4,8 @@ console.log(questions[0].nextElementSibling);
 for(i = 0; i< questions.length;i++){
   questions[i].i = i;
     questions[i].addEventListener("click", function(e) {
-        console.log(e.target);
-
-
+      
         panel = (e.target).nextElementSibling; 
-        console.log(panel);
- 
-        // issue is panel i think... like this isn't selecting properly cus error is that 
-        // .style is undefined so i think isn't selecting thing want...
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-          } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-          }
+        panel.classList.toggle("hidden")
     })
 }
